a94cbf5cf6cc3f79ae4a6ffda1fe7f295a205e36
Tim Chase <timjchase@gmail.com>
2020-04-03 10:54:48 -0400

    Completed Chapter4

--- mb_project/posts/views.py
+++ mb_project/posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Post
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
+    context_object_name = "all_posts_list"
+
 
 # Create your views here.
